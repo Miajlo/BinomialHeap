bf420218b88c40e84f4f0baef1c7199a15414465
Miajlo <mihajlod.mes@gmail.com>
2024-06-30 00:46:51 +0200

    functional merge and root list print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GenericBinomialHeap;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        BinomialHeap<int> heap = new();
+
+        heap.insert(1);
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(6);
+        heap.insert(7);
+        heap.insert(8);
+        heap.PrintRootList();
+
+        Console.WriteLine($"Min: {heap!.Min!.Data}");
+        Console.WriteLine($"Size: {heap.Size}");
+    }
+}
+
+
+
